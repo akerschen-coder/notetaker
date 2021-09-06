cb2bc40921e87a1bd5a15ee9c2acc24fbbaa6a2a
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-09-05 19:48:30 -0600

    server page and 404 page

---
+++ server.js
@@ -0,0 +1,29 @@
+// all the requires 
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+// port and app
+const PORT = process.env.port || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+// send to index page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+//send to notes page 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+//send to 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/404.html'))
+);
+// listener 
+app.listen(PORT);
